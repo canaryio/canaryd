407ee799c46d95fe9df0e4e12af229a8aee4d6d1
Michael Gorsuch <michael.gorsuch@gmail.com>
2014-04-17 21:29:27 +0000

    sketching out some other funcs

--- canaryd.go
+++ canaryd.go
@@ -10,8 +10,18 @@ func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, world")
 }
 
+func checks(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func measurements(res http.ResponseWriter, req *http.Request) {
+
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/checks", checks)
+
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
